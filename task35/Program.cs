b6d5cac93891ad866b7cc83265bb46377e672573
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-29 12:11:33 +0300

    task35 Количество чисел в массиве которые лежат в отрезке

---
+++ task35/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+35. Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своем решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+Console.Clear();
+int[] GetRandomArray(int arrayLength1, int start, int end)
+
+{
+    int[] array = new int[arrayLength1];
+    for (int i=0; i<array.Length; i++)
+    {
+        array [i] = new Random().Next(start, end);
+    }
+    return array;
+}
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.WriteLine("]");
+}
+int QuantityNumber(int[] array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length;i++)
+    {
+        if (array[i]>=10 && array[i]<=99)
+        {
+            count++;
+
+        }
+    }
+    return count;
+}
+System.Console.WriteLine("Введи число: ");
+int arrayL = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+int[] userarray =GetRandomArray(arrayL,min,max);
+PrintArray(userarray);
+System.Console.WriteLine(QuantityNumber(userarray));
