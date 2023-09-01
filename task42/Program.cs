28d3462b94292cbb08f573c05ecd26815721a255
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-09-01 17:55:17 +0300

    task42 binary number

---
+++ task42/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+System.Console.Write("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string BinaryNumber(int num)
+{
+    string result = string.Empty;
+    int temp=0;
+    for (int i = number; i>0; i/=2)
+    {
+        temp = i%2;
+        result = temp + result;
+        
+    }
+    return result;
+}
+string result = BinaryNumber(number) ;
+System.Console.WriteLine(result);
