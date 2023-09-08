bea57c4e2480ea610e9119c60d63130eb1cf4898
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-09-08 21:32:57 +0300

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+Console.Clear();
+int[,] GetRandom2DArray(int rows, int cols, int deviation )
+{
+    int[,] result = new int[rows,cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result[i,j] = new Random().Next(-deviation,deviation);
+            
+        }
+        
+    }
+    return result;
+}
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+        
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j]+"\t");
+            
+        }
+        if (arrayToPrint.GetLength(0)-i!=1)
+        {
+            System.Console.WriteLine();
+        }
+        
+    }
+}
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.WriteLine("]");
+}
+void SumInLines(int[,] numbers)
+{
+    int[] sumArray = new int[numbers.GetLength(0)];
+     Console.Write("\nСуммы элементов в каждой строке: ");
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            sumArray[i] += numbers[i, j];
+        }
+        Console.Write($"{sumArray[i]} ");
+    }
+        int minrows = 0;
+    for (int i = 0; i < sumArray.Length; i++)
+    {
+        if (sumArray[minrows] > sumArray[i]) minrows = i;
+    }
+    Console.Write($"\n{minrows} строка");
+}
+System.Console.Write("Введите количество строк двумерного массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов двумерного массива: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = GetRandom2DArray(rows,cols,10);
+Print2DArray(array);
+System.Console.WriteLine();
+ SumInLines(array);
