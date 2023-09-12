069773b40e866df6bd0e8961a806b19080a24cbe
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-09-12 19:58:21 +0300

    task69

---
+++ task69/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+69. Напишите программу, которая на вход принимает 
+два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+*/
+int exponnum=ExponNumber(2,3);
+int ExponNumber(int a, int b)
+{
+    if (b==1)
+    {
+        return a;
+    }
+    else
+    {
+        return a*ExponNumber(a,b-1);
+    }
+}
+System.Console.WriteLine(exponnum);
