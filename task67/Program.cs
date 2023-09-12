93d5692a571d9117eca633a825ad9e8762ff1afe
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-09-12 19:44:14 +0300

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+67. Напишите программу, которая будет принимать на вход число и возвращать сумму 
+его цифр.
+*/
+int result = SumNumber(11);
+int SumNumber(int n)
+{
+    if (n==0)
+    {
+       return 0; 
+  
+    }
+    else
+    {
+        return n%10+SumNumber(n/10);
+
+    }
+}
+System.Console.WriteLine(result);
